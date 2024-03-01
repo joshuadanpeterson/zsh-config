67b5590b86d789450e235e634cafc161dc9cc1d6
Josh Peterson <joshuadanpeterson@gmail.com>
2024-02-29 23:09:28 -0700

    Added Obsidian aliases

--- config/aliases.zsh
+++ config/aliases.zsh
@@ -1,7 +1,7 @@
 # Aliases
 
 # Change Directory to NucampFolder NodeJS
-alias nc='cd /Users/joshpeterson/Dropbox/programming/education/NucampFolder/WebDev/5-NodeJS-Express-MongoDB/ && ls'
+alias nc='cd $HOME/Dropbox/programming/education/NucampFolder/WebDev/5-NodeJS-Express-MongoDB/ && ls'
 
 # .config/dotfiles
 alias dot='cd ~/.config/dotfiles'
@@ -10,7 +10,7 @@ alias dot='cd ~/.config/dotfiles'
 alias zshrc='nvim ~/.config/dotfiles/.zshrc'
 alias szshrc='source ~/.config/dotfiles/.zshrc'
 
-# Tmux 
+# Tmux
 alias tmxc='nvim ~/.config/tmux/.tmux.conf'
 alias tmxs='tmux source ~/.config/tmux/.tmux.conf'
 alias tmxd='cd ~/.config/tmux/'
@@ -19,6 +19,7 @@ alias tmxls='tmux list-sessions'
 alias tmxlk='tmux list-keys'
 alias tmx='tmux'
 alias tmxn='tmux attach -t base || tmux new -s base'
+alias tmxks='tmux kill-server'
 
 # Lazy Load plugins
 alias lzyld='nvim ~/.config/dotfiles/lazyload.zsh'
@@ -38,3 +39,15 @@ alias nvimd='cd ~/.config/nvim'
 alias rnd='cd ~/.config/ranger'
 alias rnc='nvim ~/.config/ranger/rc.conf'
 alias rn='ranger'
+
+# Leetcode
+alias lc='nvim leetcode.nvim'
+
+# Obsidian Vaults
+alias obl='nvim "$HOME/Dropbox/DropsyncFiles/Obsidian Vault/Blogging/"'
+alias ocr='nvim "$HOME/Dropbox/DropsyncFiles/Obsidian Vault/Crypto/"'
+alias ojo='nvim "$HOME/Dropbox/DropsyncFiles/Obsidian Vault/Journals/"'
+alias opo='nvim "$HOME/Dropbox/DropsyncFiles/Obsidian Vault/Poetry/"'
+alias opr='nvim "$HOME/Dropbox/DropsyncFiles/Obsidian Vault/Programming/"'
+alias otr='nvim "$HOME/Dropbox/DropsyncFiles/Obsidian Vault/Temporal Rift/"'
+alias oud='nvim "$HOME/Dropbox/DropsyncFiles/Obsidian Vault/Udemy/"'
